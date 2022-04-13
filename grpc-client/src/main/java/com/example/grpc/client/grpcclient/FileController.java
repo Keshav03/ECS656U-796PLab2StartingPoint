450731eeb13f2839586fdf98d3a3d59549c8e126
KeshavBokhoree <60392137+Keshav03@users.noreply.github.com>
2022-04-13 23:19:02 +0100

    Create FileController.java

---
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/FileController.java
@@ -0,0 +1,62 @@
+import com.techgeeknext.util.FileUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileUploadController {
+
+    /**
+     * Method to upload multiple files
+     * @param files
+     * @return FileResponse
+     */
+    @PostMapping("/upload")
+    public String uploadFiles(@RequestParam("files") MultipartFile[] files) {
+        try {
+            createDirIfNotExist();
+
+            List<String> fileNames = new ArrayList<>();
+
+            // read and write the file to the local folder
+            Arrays.asList(files).stream().forEach(file -> {
+                byte[] bytes = new byte[0];
+                try {
+                    bytes = file.getBytes();
+                    Files.write(Paths.get(FileUtil.folderPath + file.getOriginalFilename()), bytes);
+                    fileNames.add(file.getOriginalFilename());
+                } catch (IOException e) {
+
+                }
+            });
+
+            return "Files uploaded sucessfully! "
+
+        } catch (Exception e) {
+            return "Error in uploading files."
+        }
+    }
+
+    /**
+     * Create directory to save files, if not exist
+     */
+    private void createDirIfNotExist() {
+        //create directory to save the files
+        File directory = new File(FileUtil.folderPath);
+        if (! directory.exists()){
+            directory.mkdir();
+        }
+    }
+}
